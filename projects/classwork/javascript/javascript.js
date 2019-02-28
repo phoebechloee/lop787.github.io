2b6cf8f34a0c6c39bba0be61efd2e1e45ffecae1
Phoebe Lo <lop787@newschool.edu>
2019-02-27 21:40:48 -0500

    javascript

---
+++ projects/classwork/javascript/javascript.js
@@ -0,0 +1,104 @@
+var my_name = 'phoebe';
+console.log(my_name);
+
+var number = '5'
+console.log(number);
+console.log('5');
+
+console.log(false);
+var name_spell =['P', 'H', 'O', 'E', 'B', 'E'];
+console.log(name_spell.length);
+
+console.log(['What', 1+5, true]);
+console.log(['Phoebe', 'Lo']);
+console.log(50+50);
+
+var word = ['supercalifragilisticexpialidocious'];
+console.log(word.length)
+
+var students = [
+  'Amaya',
+  'Sierra',
+  'Colin',
+  'Trudy',
+  'Aeva',
+  'Maddie',
+  'Scarlet',
+  'Phoebe',
+  'Cecilia',
+  'Gavin',
+  'Aileen',
+  'Simran',
+  'Julia',
+  'Amal',
+  'Mary',
+  'Xiyue'
+];
+
+if (student = 16) {
+console.log('all the students are in class');
+} else {
+  console.log('not everyone is here');
+}
+
+for ( var i = 0; i < name_spell.length; i++ ) {
+  console.log( name_spell[i] + ' is in index ' + i );
+}
+
+var time = Date.getTime()
+if (time = 19:00:00-21:40:00) {
+console.log('class is not over');
+}
+
+var date = Date.getDate():
+console.log(date);
+
+
+var day = Date.getDay():
+if (day = M) {
+  console.log('Why do people hate Mondays?')
+}
+
+var day = getDay():
+if (day = TUE) {
+  console.log('Second day of the week')
+}
+
+var day = getDay():
+if (day = WED) {
+  console.log('Hump day!')
+}
+
+var day = getDay():
+if (day = THUR) {
+  console.log('Almost friday')
+}
+
+var day = getDay():
+if (day = FRI) {
+  console.log('TGIF')
+}
+
+var day = getDay():
+if (day = SAT) {
+  console.log('Saturdays were my favourite as a kid')
+}
+
+var day = getDay():
+if (day = SUN) {
+  console.log('Yes, tomorrow is Monday')
+}
+
+var fave_foods = ['sushi', 'chips', 'ramen', 'okra'];
+console.log(fave_foods);
+
+var fave_animals = ['sloth', 'cat', 'giraffe', 'tiger'];
+console.log(fave_animals);
+
+var fave_places = ['hong kong', 'new york', 'japan', 'home'];
+console.log(fave_places);
+
+var fave_pokemons = ['charmander', 'mewtwo', 'bidoof', 'incineroar'];
+console.log(fave_pokemons);
+
+var faves = [fave_pokemons, fave_place, fave_animals, fave_foods]
